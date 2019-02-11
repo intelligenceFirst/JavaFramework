a61461b671f40c1abd7e3e6b804b54f8cf9ecaf5
james <jameshulsey1986@gmail.com>
2019-02-10 19:04:09 -0600

    Added execute method to get XML document.

--- src/main/java/API/IFRequest.java
+++ src/main/java/API/IFRequest.java
@@ -4,6 +4,12 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.StringReader;
 
 /**
  * Created by james on 2/10/2019.
@@ -16,6 +22,10 @@ public class IFRequest extends Unirest {
         _endpoint = endpoint;
     }
 
+    /**
+     * <b>Description:</b> Executes a Unirest HTTP post and gets a response as a string.
+     * @return
+     */
     public HttpResponse<String> executePostAndGetString() {
         HttpResponse<String> response = null;
 
@@ -28,12 +38,39 @@ public class IFRequest extends Unirest {
         return response;
     }
 
+    /**
+     * <b>Description:</b> Executes a Unirest HTTP post and gets a response as a JSON object.
+     * @return
+     */
     public HttpResponse<JsonNode> executePostAndGetJson() {
         HttpResponse<JsonNode> response = null;
 
         try {
             response = Unirest.post(_endpoint).asJson();
         } catch (UnirestException e) {
+            //TODO: add log message here
+        }
+
+        return response;
+    }
+
+    /**
+     * <b>Description:</b> Executes a Unirest HTTP post and gets a response as an XML document.
+     * @return
+     */
+    public Document executePostAndGetXml() {
+        Document response = null;
+
+        try {
+            String stringResponse = Unirest.post(_endpoint).asString().getBody();
+
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = dbf.newDocumentBuilder();
+            InputSource is = new InputSource(new StringReader(stringResponse));
+            response = builder.parse(is);
+        } catch (UnirestException e) {
+            //TODO: add log message here
+        } catch (Exception e) {
             //TODO: add log message here
         }
 
